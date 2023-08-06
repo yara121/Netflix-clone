7eab7fae6ded154dc25dd1aa83eaf7fe4154e4cc
yara121 <yara767@gmail.com>
2023-08-06 17:19:12 +0300

    created jumbotrones and their styles and added them to app.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "normalize.css"
 
 import App from "./app"
+import { GlobalStyles } from "./global-styles"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+  <>
+    <GlobalStyles /> <App />
+  </>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
