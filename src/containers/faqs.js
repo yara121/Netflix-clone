9ab2dc9722d0e8a180dc3e970f62f54624628eb7
yara121 <yara767@gmail.com>
2023-08-10 17:15:53 +0300

    created opt-form and styled it

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Accordion } from "../components"
 import faqsData from "../fixtures/faqs.json"
+import OptForm from "../components/opt-form"
 
 export function FaqsContainer() {
   return (
@@ -13,6 +14,14 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+      <OptForm>
+        <OptForm.Input placeholder='Email address' />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   )
 }
