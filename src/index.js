c45d55cc28d4a26ceab505b77fa41a651b135266
yara121 <yara767@gmail.com>
2023-08-12 20:07:21 +0300

    created the login form

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import { render, ReactDOM } from "react-dom"
 import "normalize.css"
 
 import App from "./app"
 import { GlobalStyles } from "./global-styles"
 import { firebase } from "./lib/firebase.prod"
 import { FirebaseContext } from "./context/firebase"
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <>
+render(
+  <React.StrictMode>
     <FirebaseContext.Provider value={{ firebase }}>
-      <GlobalStyles /> <App />
+      <GlobalStyles />
+      <App />
     </FirebaseContext.Provider>
-  </>
-)
+  </React.StrictMode>,
+  document.getElementById("root")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  // const root = ReactDOM.createRoot(document.getElementById("root"))
+  // root.render(
+  //   <>
+  //     <FirebaseContext.Provider value={{ firebase }}>
+  //       <GlobalStyles /> <App />
+  //     </FirebaseContext.Provider>
+  //   </>
+)
