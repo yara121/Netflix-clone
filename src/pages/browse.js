422d8439652648d3120971669f995c7f2b53dd5c
yara121 <yara767@gmail.com>
2023-08-16 12:45:32 +0300

    created the login functionality and the user profiles

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { useContent } from "../hooks"
+import selectionFilter from "../utils/selection-filter"
+import { BrowseContainer } from "../containers/browse"
 
 export default function Browse() {
-  return <p> Hello from the browse! </p>
+  const { series } = useContent("series")
+  const { films } = useContent("films")
+  const slides = selectionFilter({ series, films })
+  console.log(slides)
+  return <BrowseContainer slides={slides} />
 }
